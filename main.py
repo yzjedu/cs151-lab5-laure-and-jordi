8426183aed8006fe76828ce781215b5775171924
LaurePatera <ampatera@loyola.edu>
2024-10-14 17:03:01 -0400

    changed code

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-
-
-
-
-
+option = ''
+balance = 1000
 SENTINEL = 'e'
-balance = 100
+while option != SENTINEL:
+    option = input('Would you like to deposit, withdraw, view balance, or exit? For deposit, enter D. For withdraw enter W. For view balance enter V, and for exit, enter E.')
+    option = option.lower()
+    while option != 'd' and option != 'w' and option != 'v':
+        print('That is an invalid input.')
+        option = input('Would you like to deposit, withdraw, view balance, or exit? For deposit, enter D. For withdraw enter W. For view balance enter V, and for exit, enter E.')
+        option = option.lower()
+    if option == 'd':
+        deposit_amount = float(input('How much money would you like to deposit?'))
+        while deposit_amount < 0:
+            print('That is an invalid amount. Please try again.')
+            deposit_amount = float(input('How much money would you like to deposit?'))
+        balance = deposit_amount + balance
+    if option == 'w':
+        withdraw_amount = float(input('How much money would you like to withdraw?'))
+        while withdraw_amount < 0:
+            print('That is an invalid amount. Please try again.')
+            withdraw_amount = float(input('How much money would you like to withdraw?'))
+        if withdraw_amount > balance:
+            print('This will make your balance negative, and you will be charged 5% interest')
+        balance = balance - withdraw_amount
+    if option == 'v':
+        print('Your balance is:', balance)
+
